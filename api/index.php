5f61e17dcb2c06c9abdf564daddc629102d5ef9e
ahmedyassin1191 <Ahmed.yassin@igate.sa>
2026-02-01 16:44:07 +0200

    debug vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,3 @@
 <?php
-// Forward Vercel requests to Laravel's public/index.php
-require __DIR__ . '/../public/index.php';
+echo "Hello from Vercel PHP!";
+// require __DIR__ . '/../public/index.php';
