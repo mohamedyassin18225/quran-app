f7bc8f0090eb3d936c985412be7ccf0489cafa9c
ahmedyassin1191 <Ahmed.yassin@igate.sa>
2026-02-01 16:54:02 +0200

    fix vercel storage path

--- api/index.php
+++ api/index.php
@@ -1,6 +1,22 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-echo "Hello from Vercel PHP! (Debug Mode)";
-// require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// === VERCEL FIX: Use /tmp for storage because Vercel is Read-Only ===
+$app->useStoragePath('/tmp');
+// ====================================================================
+
+$app->handleRequest(Request::capture());
